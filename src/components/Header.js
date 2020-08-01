4e91c77e3f76c6b72ac68f5b84b401977a7220db
Lassi Timoskainen <timoskainen.lassi@gmail.com>
2020-08-02 00:39:33 +0300

    Added a basic header

---
+++ src/components/Header.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import './Header.css'
+
+const Header = () => {
+    return (
+        <div className='Header'>
+            <header>
+                <h1>Curriculum Vitae</h1>
+                <p style={{color: 'red'}}>Lassi Timoskainen</p>
+            </header>
+        </div>
+    )
+}
+
+export default Header
