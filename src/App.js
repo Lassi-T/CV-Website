72e6740f0005d25c781bd8ae4cb4989a11438ca2
Lassi Timoskainen <timoskainen.lassi@gmail.com>
2020-08-02 15:59:55 +0300

    Added routing

--- src/App.js
+++ src/App.js
@@ -1,14 +1,23 @@
 import React from 'react'
-import NavBar from './components/NavBar'
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom'
+import Header from './components/Header'
+import Home from './components/Home'
+import Contact from './components/Contact'
 import 'bootstrap/dist/css/bootstrap.min.css'
 import './App.css'
 
 const App = () => {
-  return (
-    <div className='body'>
-      <NavBar />
-    </div>
-  )
+    return (
+        <Router>
+            <div className="body">
+                <Header />
+                <Switch>
+                    <Route path="/" exact component={Home} />
+                    <Route path="/contact" component={Contact} />
+                </Switch>
+            </div>
+        </Router>
+    )
 }
 
 export default App
--- src/components/Header.js
+++ src/components/Header.js
@@ -1,18 +1,18 @@
 import React from 'react'
 import { Navbar } from 'react-bootstrap'
 
-const NavBar = () => {
-    return(
-        <Navbar bg='dark' variant='dark'>
-            <Navbar.Brand href='#home'>LassiT</Navbar.Brand>
+const Header = () => {
+    return (
+        <Navbar bg="dark" variant="dark">
+            <Navbar.Brand href="/">LassiT</Navbar.Brand>
             <Navbar.Toggle />
             <Navbar.Collapse className="justify-content-end">
                 <Navbar.Text>
-                    <a href="#contact">Contact</a>
+                    <a href="/contact">Contact</a>
                 </Navbar.Text>
             </Navbar.Collapse>
         </Navbar>
     )
 }
 
-export default NavBar
+export default Header
