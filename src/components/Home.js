93ded01754d8128be70d13b6e7611f6f83463710
Lassi Timoskainen <timoskainen.lassi@gmail.com>
2020-08-02 19:44:46 +0300

    Added basic styling

--- src/components/Home.js
+++ src/components/Home.js
@@ -5,26 +5,26 @@ import ProfilePic from '../images/TutorKuva5.png'
 const Home = () => {
   return (
     <Container>
-      <Row style={{ padding: '20px' }}>
+      <Row style={{ padding: '20px', paddingBottom: '30px' }}>
         <h1>Home Page</h1>
       </Row>
       <Row>
-        <Col style={{ padding: '20px' }}>
-          <Image src={ProfilePic} width={200} height={200} alt='ProfilePic' roundedCircle />
+        <Col sm={3} style={{ padding: '20px', paddingTop: '0px' }}>
+          <Image src={ProfilePic} style={{border: '7px white solid'}} width={220} height={220} alt='ProfilePic' roundedCircle />
         </Col>
-      </Row>
-      <Row>
-        <Media>
-          <Media.Body>
-            <h5>About me</h5>
-            <p>
-              Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc leo quam, pharetra sit amet luctus
-              in, lacinia nec ante. Etiam nisl diam, commodo vitae orci eget, mollis fermentum lacus. Donec at
-              nibh sem. Ut egestas sodales lobortis. Praesent interdum dolor eu nunc auctor posuere. Ut
-              scelerisque quam eu purus rutrum pellentesque.
-            </p>
-          </Media.Body>
-        </Media>
+        <Col sm={8}>
+          <Media style={{ padding: '2em' }}>
+            <Media.Body>
+              <h5>About me</h5>
+              <p>
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc leo quam, pharetra sit amet
+                luctus in, lacinia nec ante. Etiam nisl diam, commodo vitae orci eget, mollis fermentum lacus.
+                Donec at nibh sem. Ut egestas sodales lobortis. Praesent interdum dolor eu nunc auctor
+                posuere. Ut scelerisque quam eu purus rutrum pellentesque.
+              </p>
+            </Media.Body>
+          </Media>
+        </Col>
       </Row>
     </Container>
   )
