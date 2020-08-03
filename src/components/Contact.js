72e04808aaf7b3188e2327acc23cd4dc83e8a5f8
Lassi Timoskainen <timoskainen.lassi@gmail.com>
2020-08-03 20:33:55 +0300

    Added v.1. of contact page

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,9 +1,44 @@
 import React from 'react'
+import { Container, Col, Row, Image } from 'react-bootstrap'
+import TelegramPic from '../images/telegram.png'
+import EmailPic from '../images/email.png'
+import PhonePic from '../images/phone.png'
+import './Contact.css'
 
 const Contact = () => {
   return (
     <div>
-      <p>My Contact information</p>
+      <Container>
+        <Row>
+          <Col>
+            <h4 className='title'>Contact me</h4>
+          </Col>
+        </Row>
+        <Row>
+          <Col>
+            <div className='infoBox'>
+              <Image className='imageDiv' src={TelegramPic} width={160} height={160} alt='Telegram picture'></Image>
+              <h4 className='textDiv'>Telegram</h4>
+              <p className='subTextDiv'>@LassiT</p>
+            </div>
+          </Col>
+          <Col>
+            <div className='infoBox'>
+            <Image className='imageDiv' src={EmailPic} width={160} height={160} alt='Telegram picture'></Image>
+              <h4 className='textDiv'>Email</h4>
+              <p className='subTextDiv'>timoskainen.lassi(at).com</p>
+              <p className='subTextDiv'>lassi.timoskainen(at)tuni.fi</p>
+            </div>
+          </Col>
+          <Col>
+            <div className='infoBox'>
+            <Image className='imageDiv' src={PhonePic} width={160} height={160} alt='Telegram picture'></Image>
+              <h4 className='textDiv'>Phone</h4>
+              <p className='subTextDiv'>+358 044 XXX XXXX</p>
+            </div>
+          </Col>
+        </Row>
+      </Container>
     </div>
   )
 }
