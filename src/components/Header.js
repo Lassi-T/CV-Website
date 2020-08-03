0f9bc464c52bee0bd70022975a778e9052f05771
Lassi Timoskainen <timoskainen.lassi@gmail.com>
2020-08-03 18:18:34 +0300

    Finished v.1. of frontpage

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,5 +1,6 @@
 import React from 'react'
-import { Navbar } from 'react-bootstrap'
+import { Navbar, Nav } from 'react-bootstrap'
+import './Header.css'
 
 const Header = () => {
   return (
@@ -7,8 +8,17 @@ const Header = () => {
       <Navbar.Brand href='/'>LassiT</Navbar.Brand>
       <Navbar.Toggle />
       <Navbar.Collapse className='justify-content-end'>
+        <Nav className='mr-auto'>
+          <Nav.Link href='/cv'>CV</Nav.Link>
+          <a className='navLink' href='https://github.com/Lassi-T' target='_blank' rel='noopener noreferrer'>
+            GitHub
+          </a>
+          <Nav.Link href='/media'>Media</Nav.Link>
+        </Nav>
         <Navbar.Text>
-          <a href='/contact'>Contact</a>
+          <a className='navLink' href='/contact'>
+            Contact
+          </a>
         </Navbar.Text>
       </Navbar.Collapse>
     </Navbar>
--- src/components/Home.js
+++ src/components/Home.js
@@ -1,22 +1,29 @@
 import React from 'react'
 import { Container, Col, Row, Media, Image } from 'react-bootstrap'
 import ProfilePic from '../images/TutorKuva5.png'
+import './Home.css'
 
 const Home = () => {
   return (
     <Container>
-      <Row style={{ padding: '20px', paddingBottom: '30px' }}>
-        <h1>Home Page</h1>
+      <Row>
+        <Col className='imageDiv'>
+          <Image
+            src={ProfilePic}
+            className='whiteBorder'
+            width={250}
+            height={250}
+            alt='ProfilePic'
+            roundedCircle
+          />
+        </Col>
       </Row>
       <Row>
-        <Col sm={3} style={{ padding: '20px', paddingTop: '0px' }}>
-          <Image src={ProfilePic} style={{border: '7px white solid'}} width={220} height={220} alt='ProfilePic' roundedCircle />
-        </Col>
-        <Col sm={8}>
-          <Media style={{ padding: '2em' }}>
+        <Col>
+          <Media>
             <Media.Body>
-              <h5>About me</h5>
-              <p>
+              <h5 className='title'>Lassi Timoskainen</h5>
+              <p className='textBox'>
                 Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc leo quam, pharetra sit amet
                 luctus in, lacinia nec ante. Etiam nisl diam, commodo vitae orci eget, mollis fermentum lacus.
                 Donec at nibh sem. Ut egestas sodales lobortis. Praesent interdum dolor eu nunc auctor
