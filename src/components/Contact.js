b119567b339f26cd08d654daa93d0757ba19de6f
Lassi Timoskainen <timoskainen.lassi@gmail.com>
2020-08-04 18:30:58 +0300

    Finnished v.2. of frontpage

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,45 +1,45 @@
 import React from 'react'
-import { Container, Col, Row, Image } from 'react-bootstrap'
+import { Container, Col, Row, Image, Media } from 'react-bootstrap'
 import TelegramPic from '../images/telegram.png'
 import EmailPic from '../images/email.png'
 import PhonePic from '../images/phone.png'
 import './Contact.css'
 
+// Make contact boxes flow on top of each other!
+
 const Contact = () => {
   return (
-    <div>
-      <Container>
-        <Row>
-          <Col>
-            <h4 className='title'>Contact me</h4>
-          </Col>
-        </Row>
-        <Row>
-          <Col>
-            <div className='infoBox'>
-              <Image className='imageDiv' src={TelegramPic} width={160} height={160} alt='Telegram picture'></Image>
-              <h4 className='textDiv'>Telegram</h4>
-              <p className='subTextDiv'>@LassiT</p>
-            </div>
-          </Col>
-          <Col>
-            <div className='infoBox'>
+    <Container>
+      <Row>
+        <Col>
+          <h4 className='title'>Contact me</h4>
+        </Col>
+      </Row>
+      <Row>
+        <Col>
+          <Media.Body className='infoBox'>
+            <Image className='imageDiv' src={TelegramPic} alt='Telegram picture'></Image>
+            <h4 className='textDiv'>Telegram</h4>
+            <p className='subTextDiv'>@LassiT</p>
+          </Media.Body>
+        </Col>
+        <Col>
+          <Media.Body className='infoBox'>
             <Image className='imageDiv' src={EmailPic} width={160} height={160} alt='Telegram picture'></Image>
-              <h4 className='textDiv'>Email</h4>
-              <p className='subTextDiv'>timoskainen.lassi(at).com</p>
-              <p className='subTextDiv'>lassi.timoskainen(at)tuni.fi</p>
-            </div>
-          </Col>
-          <Col>
-            <div className='infoBox'>
+            <h4 className='textDiv'>Email</h4>
+            <p className='subTextDiv'>timoskainen.lassi(at).com</p>
+            <p className='subTextDiv'>lassi.timoskainen(at)tuni.fi</p>
+          </Media.Body>
+        </Col>
+        <Col>
+          <Media.Body className='infoBox'>
             <Image className='imageDiv' src={PhonePic} width={160} height={160} alt='Telegram picture'></Image>
-              <h4 className='textDiv'>Phone</h4>
-              <p className='subTextDiv'>+358 044 XXX XXXX</p>
-            </div>
-          </Col>
-        </Row>
-      </Container>
-    </div>
+            <h4 className='textDiv'>Phone</h4>
+            <p className='subTextDiv'>+358 044 XXX XXXX</p>
+          </Media.Body>
+        </Col>
+      </Row>
+    </Container>
   )
 }
 
--- src/components/Home.js
+++ src/components/Home.js
@@ -1,6 +1,9 @@
 import React from 'react'
 import { Container, Col, Row, Media, Image } from 'react-bootstrap'
 import ProfilePic from '../images/TutorKuva5.png'
+import GitHubPic from '../images/GitHub-Mark-Light-120px-plus.png'
+import LinkedinPic from '../images/LI-In-Bug.png'
+import InstagramPic from '../images/instagram.png'
 import './Home.css'
 
 const Home = () => {
@@ -21,15 +24,21 @@ const Home = () => {
       <Row>
         <Col>
           <Media>
-            <Media.Body>
-              <h5 className='title'>Lassi Timoskainen</h5>
-              <p className='textBox'>
-                Welcome to my website!
-                <br />
-                I made this online portfolio site using React, utilizing some Reactboostrap components.
-                <br />
-                Here you can find my CV, my contact information, a link to my GitHub page and some media.
-              </p>
+            <Media.Body className='textBox'>
+              <h5 className='mainTitle'>Lassi Timoskainen</h5>
+              <div className='divider'></div>
+              <p className='text'>Python | C/C++ | JavaScript | React | NodeJS</p>
+              <div className='imageRow'>
+                <a href='https://github.com/Lassi-T' target='_blank' rel='noopener noreferrer'>
+                  <Image src={GitHubPic} width={40} height={40} />
+                </a>
+                <a className='centerImage' href='https://www.linkedin.com/in/lassi-timoskainen-73b0261b4/' target='_blank' rel='noopener noreferrer'>
+                  <Image src={LinkedinPic} width={40} height={40} />
+                </a>
+                <a className='rightImage' href='https://www.instagram.com/lassii/?hl=fi' target='_blank' rel='noopener noreferrer'>
+                  <Image src={InstagramPic} width={40} height={40} />
+                </a>
+              </div>
             </Media.Body>
           </Media>
         </Col>
