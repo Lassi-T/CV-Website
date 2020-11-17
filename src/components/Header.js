8e21a145955c605c8ab58f0195d1247202ff2384
Lassi Timoskainen <timoskainen.lassi@gmail.com>
2020-11-17 18:06:46 +0200

    Updated repo

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,7 +1,7 @@
 import React from 'react'
 import { Navbar, Nav } from 'react-bootstrap'
 import './Header.css'
-import CVPic from '../images/cvpic.jpg'
+import CVPic from '../images/cvpic.png'
 
 const Header = () => {
   return (
